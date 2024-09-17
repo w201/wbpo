7ead1e02c22130efc821e5e82cb4cde261dd93bf
android <android@>
2024-09-17 16:36:50 +0200

    changed color of favorites button

--- app/src/main/java/one/codium/wbpo/ui/theme/Theme.kt
+++ app/src/main/java/one/codium/wbpo/ui/theme/Theme.kt
@@ -15,13 +15,13 @@ import one.codium.wbpo.core.entity.ThemeMode
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Red80
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Red20
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
